bdeddac8689010de60e271a9d9439fe6485d9279
futurum-dev <me@futurum.dev>
2022-02-06 15:21:25 +0000

    Allow IStartable to work with Host

---
+++ sample/Futurum.Microsoft.Extensions.DependencyInjection.Sample/SampleStartable.cs
@@ -0,0 +1,18 @@
+﻿using ILogger = Serilog.ILogger;
+
+namespace Futurum.Microsoft.Extensions.DependencyInjection.Sample;
+
+public class SampleStartable : IStartable
+{
+    private readonly ILogger _logger;
+
+    public SampleStartable(ILogger logger)
+    {
+        _logger = logger;
+    }
+    
+    public void Start()
+    {
+        _logger.Information($"{nameof(SampleStartable)} started");
+    }
+}
